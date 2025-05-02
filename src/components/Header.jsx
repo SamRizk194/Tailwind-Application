28ecf5d3351bc6eb66d60a4a4ab10dde78f287a2
unknown <samehrizk1993@gmail.com>
2025-05-02 15:37:00 +0300

    finishing navbar sec

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,60 @@
+import { useEffect, useRef } from "react";
+
 function Header() {
+  const headRef = useRef();
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scrollY > 100) {
+        headRef.current.style.background = "#0c1524";
+        headRef.current.style.padding = "20px 0";
+      } else {
+        headRef.current.style.background = "transparent";
+        headRef.current.style.padding = "60px";
+      }
+    });
+  }, []);
   return (
-    <header>
-      <a href="/">
-        <img
-          src="../../src/assets/images/logo.svg"
-          alt="logo"
-          className="bg-"
-        />
-      </a>
-      <nav>
-        <ul>
-          <li>
-            <a href="/features" className="text-white opacity-[0.9]">
-              Features
-            </a>
-          </li>
-          <li>
-            <a href="/team" className="text-white opacity-[0.9]">
-              Team
-            </a>
-          </li>
-          <li>
-            <a href="signin" className="text-white opacity-[0.9]">
-              Signin
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <header
+      ref={headRef}
+      className="pt-[60px] fixed top-0 left-0 w-full z-50 transition-all duration-200"
+    >
+      <div className="container mx-auto flex justify-between items-center gap-[30px] sm:gap-0 flex-col sm:flex-row">
+        <a href="/">
+          <img
+            src="../../src/assets/images/logo.svg"
+            alt="logo"
+            className="bg-"
+          />
+        </a>
+        <nav>
+          <ul className="flex gap-[50px]">
+            <li>
+              <a
+                href="/features"
+                className="text-white opacity-[0.9] hover:underline hover:opacity-[1] transition-opacity duration-200"
+              >
+                Features
+              </a>
+            </li>
+            <li>
+              <a
+                href="/team"
+                className="text-white opacity-[0.9] hover:underline"
+              >
+                Team
+              </a>
+            </li>
+            <li>
+              <a
+                href="signin"
+                className="text-white opacity-[0.9] hover:underline"
+              >
+                Signin
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        maincolor: "red",
+      screens: {
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        "2xl": "1400px",
       },
     },
   },
