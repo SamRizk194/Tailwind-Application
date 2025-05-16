57a7b3c49c1cf61b1e034f30d56d6792da3a7d40
unknown <samehrizk1993@gmail.com>
2025-05-16 23:06:58 +0300

    modifing images urrl

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,10 @@
-import { useState } from "react";
 import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 import iconlocation from "../assets/images/icon-location.svg";
 import logo from "../assets/images/logo.svg";
+import phone from "../assets/images/icon-phone.svg";
+import email from "../assets/images/icon-email.svg";
 
 function Footer() {
-  const [contact] = useState([
-    { icon: "icon-phone.svg", text: "+20121212121" },
-    { icon: "icon-email.svg", text: "example@fylo.com" },
-  ]);
-  const [links] = useState([
-    "About Us",
-    "Contact Us",
-    "Jobs",
-    "Terms",
-    "Press",
-    "Privacy",
-    "Blog",
-  ]);
-  const [socialIcons] = useState(["facebook", "twitter", "instagram"]);
-
   return (
     <section className="bg-[#0c1524] pt-[320px] md:pt-[200px] pb-[100px] text-white w-screen">
       <div className="container px-10">
@@ -42,54 +28,122 @@ function Footer() {
               fuga.
             </p>
           </div>
+
           <div>
-            {contact.map((item) => (
-              <div
-                key={item.text}
-                className="flex items-center gap-[15px] mb-[15px] last-of-type:mb-0"
-              >
-                <img
-                  src={`/src/assets/images/${item.icon}`}
-                  alt="icon"
-                  className="w-[18px] h-[18px] object-contain"
-                />
-                <p>{item.text}</p>
-              </div>
-            ))}
+            <div className="flex items-center gap-[15px] mb-[15px]">
+              <img
+                src={phone}
+                alt="phone"
+                className="w-[18px] h-[18px] object-contain"
+              />
+              <p>+20121212121</p>
+            </div>
+            <div className="flex items-center gap-[15px]">
+              <img
+                src={email}
+                alt="email"
+                className="w-[18px] h-[18px] object-contain"
+              />
+              <p>example@fylo.com</p>
+            </div>
           </div>
+
           <ul className="grid grid-cols-1 md:grid-cols-2 gap-[20px]">
-            {links.map((item) => (
-              <li key={item}>
-                <a
-                  href={`/${item.toLowerCase().replace(/\s+/g, "-")}`}
-                  className="hover:text-primary transition-all duration-200 text-base"
-                >
-                  {item}
-                </a>
-              </li>
-            ))}
+            <li>
+              <a
+                href="/about-us"
+                className="hover:text-primary transition-all duration-200 text-base"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="/contact-us"
+                className="hover:text-primary transition-all duration-200 text-base"
+              >
+                Contact Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="/jobs"
+                className="hover:text-primary transition-all duration-200 text-base"
+              >
+                Jobs
+              </a>
+            </li>
+            <li>
+              <a
+                href="/terms"
+                className="hover:text-primary transition-all duration-200 text-base"
+              >
+                Terms
+              </a>
+            </li>
+            <li>
+              <a
+                href="/press"
+                className="hover:text-primary transition-all duration-200 text-base"
+              >
+                Press
+              </a>
+            </li>
+            <li>
+              <a
+                href="/privacy"
+                className="hover:text-primary transition-all duration-200 text-base"
+              >
+                Privacy
+              </a>
+            </li>
+            <li>
+              <a
+                href="/blog"
+                className="hover:text-primary transition-all duration-200 text-base"
+              >
+                Blog
+              </a>
+            </li>
           </ul>
+
           <ul className="flex gap-[15px] w-full justify-center md:w-auto">
-            {socialIcons.map((item) => (
-              <li key={item}>
-                <a
-                  href={`https://www.${item}.com`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="group"
-                >
-                  <div className="w-[40px] h-[40px] flex items-center justify-center border border-white rounded-[50%]">
-                    {item === "facebook" ? (
-                      <FaFacebookF className="group-hover:text-primary transition-all duration-200" />
-                    ) : item === "twitter" ? (
-                      <FaTwitter className="group-hover:text-primary transition-all duration-200" />
-                    ) : (
-                      <FaInstagram className="group-hover:text-primary transition-all duration-200" />
-                    )}
-                  </div>
-                </a>
-              </li>
-            ))}
+            <li>
+              <a
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group"
+              >
+                <div className="w-[40px] h-[40px] flex items-center justify-center border border-white rounded-[50%]">
+                  <FaFacebookF className="group-hover:text-primary transition-all duration-200" />
+                </div>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group"
+              >
+                <div className="w-[40px] h-[40px] flex items-center justify-center border border-white rounded-[50%]">
+                  <FaTwitter className="group-hover:text-primary transition-all duration-200" />
+                </div>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group"
+              >
+                <div className="w-[40px] h-[40px] flex items-center justify-center border border-white rounded-[50%]">
+                  <FaInstagram className="group-hover:text-primary transition-all duration-200" />
+                </div>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
