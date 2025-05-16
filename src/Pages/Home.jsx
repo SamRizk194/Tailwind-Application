b60149d9f84606cd9820d28abe978c93c36e6dff
unknown <samehrizk1993@gmail.com>
2025-05-16 03:44:37 +0300

    adding GetStarted sec

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,8 @@
-import { Fragment } from "react";
 import Landing from "../components/Landing";
 import Features from "../components/Features";
 import StayProductive from "../components/StayProductive";
 import Testimonials from "../components/Testimonials";
+import GetStarted from "../components/GetStarted";
 
 function Home() {
   return (
@@ -11,6 +11,7 @@ function Home() {
       <Features />
       <StayProductive />
       <Testimonials />
+      <GetStarted />
     </>
   );
 }
